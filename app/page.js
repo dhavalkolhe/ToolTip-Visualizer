f7c62ebc0d67f4911ae0a5dbf19db73576a6bb70
Dhaval Kolhe <72658475+dhavalkolhe@users.noreply.github.com>
2023-08-23 02:34:16 +0530

    Screen UI Implemented

--- app/page.js
+++ app/page.js
@@ -2,16 +2,19 @@
 
 import React from "react";
 import AppContextProvider from "../context/AppContextProvider";
+
 import Form from "../components/Form";
+import Screen from "../components/Screen";
 
 export default function Home({ Component, pageProps }) {
   return (
     <AppContextProvider>
-      <div className="h-[90vh] my-auto flex justify-around items-center ">
+      <div className="h-[100vh] my-auto flex justify-around items-center ">
         <div className="flex justify-center items-center p-4 bg-black bg-opacity-[15%] rounded-xl w-[45%]">
           <Form {...pageProps} />
         </div>
-        <div className="border-2">Mobile Screen</div>
+
+        <Screen />
       </div>
     </AppContextProvider>
   );
---
+++ components/Screen.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Screen() {
+  return (
+    <div className="h-[80%] flex flex-col justify-between bg-black bg-opacity-25 outline outline-[15px] outline-black p-2 rounded-xl w-[20%]">
+      <div className="btnsContainer">
+        <div>
+          <button className="btn">Button 1</button>
+        </div>
+        <div>
+          <button className="btn">Button 2</button>
+        </div>
+      </div>
+      <div className="btnsContainer">
+        <div>
+          <button className="btn">Button 3</button>
+        </div>
+      </div>
+      <div className="btnsContainer">
+        <div>
+          <button className="btn">Button 4</button>
+        </div>
+        <div>
+          <button className="btn">Button 5</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Screen;
